c26e96490107c82b26d330b3880bee7699dd8798
Zordon <65111609+Zordon1337@users.noreply.github.com>
2023-05-28 15:00:46 +0200

    Fixed Database

--- src/ClashRoyale/GameAssets/database.sql
+++ src/ClashRoyale/GameAssets/database.sql
@@ -1,22 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 28, 2023 at 03:00 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
-CREATE TABLE IF NOT EXISTS `player` (
-  `Id` bigint(20) NOT NULL,
-  `Trophies` bigint(20) NOT NULL,
-  `Language` text CHARACTER SET utf8mb4 NOT NULL,
-  `FacebookId` text CHARACTER SET utf8mb4,
-  `Home` text CHARACTER SET utf8mb4 NOT NULL,
-  `Sessions` text CHARACTER SET utf8mb4 NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `clan` (
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rrdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clan`
+--
+
+CREATE TABLE `clan` (
   `Id` bigint(20) NOT NULL,
   `Trophies` bigint(20) NOT NULL,
   `RequiredTrophies` bigint(20) NOT NULL,
   `Type` bigint(20) NOT NULL,
-  `Region` text CHARACTER SET utf8mb4 NOT NULL,
-  `Data` text CHARACTER SET utf8mb4 NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `Region` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `Data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `player`
+--
+
+CREATE TABLE `player` (
+  `Id` bigint(20) NOT NULL,
+  `Trophies` bigint(20) NOT NULL,
+  `Language` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `FacebookId` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `Home` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `Sessions` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=COMPACT;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `clan`
+--
+ALTER TABLE `clan`
+  ADD PRIMARY KEY (`Id`) USING BTREE;
+
+--
+-- Indexes for table `player`
+--
+ALTER TABLE `player`
+  ADD PRIMARY KEY (`Id`) USING BTREE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
